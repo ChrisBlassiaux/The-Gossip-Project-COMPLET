45be7510c3352c5f5235e47817d4f8f704bda6c8
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-05-07 23:48:33 +0200

    feat: comment display

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,21 +2,29 @@ class CommentsController < ApplicationController
 
   # Display a view to create a comment
   def new
-    @comment = Comment.new(gossip: "", user: User.last, content: "")
+    @comment = Comment.new
   end
 
   # Create a new comment
   def create
     gossip = Gossip.find(params[:format]).id
 
-    comment = Comment.new(gossip_id: gossip,
-                          user: User.last,
-                          content: params[:content])
+    if current_user
+      current_user_comment = current_user.id
+    else
+      current_user_comment = ""
+    end
+
+    comment = Comment.new(content: params[:content],
+                          gossip_id: gossip,
+                          user_id: current_user_comment
+                          )
 
     if comment.save
       flash[:success] = "Le commentaire a été créé avec succès."
       redirect_to gossip_path(gossip)
     else
+      flash[:echec] = "Connecte toi pour pouvoir commenter les potins."
       redirect_to gossip_path(gossip)
     end
   end
